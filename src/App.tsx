975023367b0219e666ef3c8ad62209192b558c4b
Abhishek Kumar <7abhik@gmail.com>
2026-08-18 20:46:54 +0400

    form ref Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-// import ListGroup from "./components/ListGroup";
-// let items = ["New Yourk", "Tokeyo", "New Delhi", "Patna", "Goa"];
-// let heading = "Cities";
-// const handleSelect = (item: string) => {
-//   console.log(item);
-// };
-import Button from "./components/Button";
-const handleButton = () => {
-  console.log("Button clicked");
-};
+import Form from "./components/Form";
 function App() {
-  const [isAlert, setAlert] = useState(false);
   return (
     <div>
-      {/* <ListGroup items={items} heading={heading} onSelectItem={handleSelect} /> */}
-      {isAlert && (
-        <Alert onClose={() => setAlert(false)}>
-          <span>Hello World</span>
-        </Alert>
-      )}
-      <Button
-        onClick={() => {
-          setAlert(true);
-          handleButton();
-        }}
-      >
-        My Button
-      </Button>
+      <Form />
     </div>
   );
 }
